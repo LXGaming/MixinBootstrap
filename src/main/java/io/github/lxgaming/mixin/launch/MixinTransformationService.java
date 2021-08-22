b4391dd3f4f6255911006c55327323517ae0ffe0
ZekerZhayard <trees1000@qq.com>
2021-08-22 12:56:26 +0800

    Support forge 1.17.1.

--- src/main/java/io/github/lxgaming/mixin/launch/MixinTransformationService.java
+++ src/main/java/io/github/lxgaming/mixin/launch/MixinTransformationService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Alex Thomson
+ * Copyright 2019-2021 Alex Thomson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,87 +16,36 @@
 
 package io.github.lxgaming.mixin.launch;
 
-import cpw.mods.modlauncher.LaunchPluginHandler;
 import cpw.mods.modlauncher.Launcher;
 import cpw.mods.modlauncher.api.IEnvironment;
 import cpw.mods.modlauncher.api.ITransformationService;
-import cpw.mods.modlauncher.api.ITransformer;
 import cpw.mods.modlauncher.api.IncompatibleEnvironmentException;
-import cpw.mods.modlauncher.serviceapi.ILaunchPluginService;
 import io.github.lxgaming.classloader.ClassLoaderUtils;
-import joptsimple.OptionSpecBuilder;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import javax.annotation.Nonnull;
 import java.lang.reflect.Field;
-import java.net.URL;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
-public class MixinTransformationService implements ITransformationService {
-    
-    public static final String NAME = "mixinbootstrap";
-    
-    private static final Logger LOGGER = LogManager.getLogger("MixinBootstrap Launch");
-    private final Map<String, ILaunchPluginService> launchPluginServices;
-    private final Set<ITransformationService> transformationServices;
-    
-    public MixinTransformationService() {
-        if (Launcher.INSTANCE == null) {
-            throw new IllegalStateException("Launcher has not been initialized!");
-        }
-        
-        this.launchPluginServices = getLaunchPluginServices();
-        this.transformationServices = new HashSet<>();
-    }
-    
-    @Nonnull
-    @Override
-    public String name() {
-        return MixinTransformationService.NAME;
-    }
-    
-    @Override
-    public void arguments(BiFunction<String, String, OptionSpecBuilder> argumentBuilder) {
-        this.transformationServices.forEach(transformationService -> transformationService.arguments(argumentBuilder));
-    }
-    
-    @Override
-    public void argumentValues(OptionResult option) {
-        this.transformationServices.forEach(transformationService -> transformationService.argumentValues(option));
-    }
+public class MixinTransformationService extends CommonTransformationService {
     
     @Override
     public void initialize(IEnvironment environment) {
         ensureTransformerExclusion();
         
-        this.transformationServices.forEach(transformationService -> transformationService.initialize(environment));
+        super.initialize(environment);
     }
-    
+
     @Override
     public void beginScanning(IEnvironment environment) {
         this.transformationServices.forEach(transformationService -> transformationService.beginScanning(environment));
-    }
-    
-    @Override
-    public List<Map.Entry<String, Path>> runScan(IEnvironment environment) {
-        List<Map.Entry<String, Path>> list = new ArrayList<>();
-        this.transformationServices.forEach(transformationService -> {
-            list.addAll(transformationService.runScan(environment));
-        });
-        
-        return list;
     }
     
     @Override
@@ -110,16 +59,30 @@ public class MixinTransformationService implements ITransformationService {
             throw new IncompatibleEnvironmentException("LaunchPluginServices is unavailable");
         }
         
-        if (!ITransformationService.class.getPackage().isCompatibleWith("4.0")) {
+        if (!ITransformationService.class.getPackage().isCompatibleWith("4.0") || ITransformationService.class.getPackage().isCompatibleWith("8.0")) {
             LOGGER.error("-------------------------[ ERROR ]-------------------------");
             LOGGER.error("Mixin is not compatible with ModLauncher v" + ITransformationService.class.getPackage().getImplementationVersion());
-            LOGGER.error("Ensure you are running Forge v28.1.45 or later");
+            LOGGER.error("Ensure you are running Forge v28.1.45 ~ v37.0.0");
             LOGGER.error("-------------------------[ ERROR ]-------------------------");
             throw new IncompatibleEnvironmentException("Incompatibility with ModLauncher");
         }
-        
+
+        ClassLoader contextCL = Thread.currentThread().getContextClassLoader();
         try {
-            ClassLoaderUtils.appendToClassPath(Launcher.class.getClassLoader(), getClass().getProtectionDomain().getCodeSource().getLocation().toURI().toURL());
+            Map<String, String> map = new HashMap<>();
+            map.put("create", "true");
+            FileSystem fs = FileSystems.newFileSystem(URI.create("jar:" + this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()), map);
+            Files.list(fs.getPath("META-INF", "libraries"))
+                .filter(p -> !Files.isDirectory(p) && p.getFileName().toString().endsWith(".jar"))
+                .forEach(p -> {
+                    try {
+                        ClassLoaderUtils.appendToClassPath(contextCL, Files.copy(p, Files.createTempFile("", ".jar"), StandardCopyOption.REPLACE_EXISTING).toUri().toURL());
+                    } catch (Throwable e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+            ClassLoaderUtils.appendToClassPath(contextCL, getClass().getProtectionDomain().getCodeSource().getLocation().toURI().toURL());
         } catch (Throwable ex) {
             LOGGER.error("Encountered an error while attempting to append to the class path", ex);
             throw new IncompatibleEnvironmentException("Failed to append to the class path");
@@ -127,41 +90,17 @@ public class MixinTransformationService implements ITransformationService {
         
         // Mixin
         // - Plugin Service
-        registerLaunchPluginService("org.spongepowered.asm.launch.MixinLaunchPlugin");
+        registerLaunchPluginService("org.spongepowered.asm.launch.MixinLaunchPluginLegacy", contextCL);
         
         // - Transformation Service
         // This cannot be loaded by the ServiceLoader as it will load classes under the wrong classloader
-        registerTransformationService("org.spongepowered.asm.launch.MixinTransformationService");
+        registerTransformationService("org.spongepowered.asm.launch.MixinTransformationServiceLegacy", contextCL);
         
         // MixinBootstrap
         // - Plugin Service
-        registerLaunchPluginService("io.github.lxgaming.mixin.launch.MixinLaunchPluginService");
-        
-        for (ITransformationService transformationService : this.transformationServices) {
-            transformationService.onLoad(env, otherServices);
-        }
-    }
-    
-    @Nonnull
-    @Override
-    @SuppressWarnings("rawtypes")
-    public List<ITransformer> transformers() {
-        List<ITransformer> list = new ArrayList<>();
-        this.transformationServices.forEach(transformationService -> {
-            list.addAll(transformationService.transformers());
-        });
-        
-        return list;
-    }
-    
-    @Override
-    public Map.Entry<Set<String>, Supplier<Function<String, Optional<URL>>>> additionalClassesLocator() {
-        return null;
-    }
-    
-    @Override
-    public Map.Entry<Set<String>, Supplier<Function<String, Optional<URL>>>> additionalResourcesLocator() {
-        return null;
+        registerLaunchPluginService("io.github.lxgaming.mixin.launch.MixinLaunchPluginService", contextCL);
+
+        super.onLoad(env, otherServices);
     }
     
     /**
@@ -197,99 +136,13 @@ public class MixinTransformationService implements ITransformationService {
             // no-op
         }
     }
-    
-    @SuppressWarnings("unchecked")
-    private void registerLaunchPluginService(String className) throws IncompatibleEnvironmentException {
-        try {
-            Class<? extends ILaunchPluginService> launchPluginServiceClass = (Class<? extends ILaunchPluginService>) Class.forName(className, true, Launcher.class.getClassLoader());
-            if (isLaunchPluginServicePresent(launchPluginServiceClass)) {
-                LOGGER.warn("{} is already registered", launchPluginServiceClass.getSimpleName());
-                return;
-            }
-            
-            ILaunchPluginService launchPluginService = launchPluginServiceClass.newInstance();
-            String pluginName = launchPluginService.name();
-            this.launchPluginServices.put(pluginName, launchPluginService);
-            
-            List<Map<String, String>> mods = Launcher.INSTANCE.environment().getProperty(IEnvironment.Keys.MODLIST.get()).orElse(null);
-            if (mods != null) {
-                Map<String, String> mod = new HashMap<>();
-                mod.put("name", pluginName);
-                mod.put("type", "PLUGINSERVICE");
-                String fileName = launchPluginServiceClass.getProtectionDomain().getCodeSource().getLocation().getFile();
-                mod.put("file", fileName.substring(fileName.lastIndexOf('/')));
-                mods.add(mod);
-            }
-            
-            LOGGER.debug("Registered {} ({})", launchPluginServiceClass.getSimpleName(), pluginName);
-        } catch (Throwable ex) {
-            LOGGER.error("Encountered an error while registering {}", className, ex);
-            throw new IncompatibleEnvironmentException(String.format("Failed to register %s", className));
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-    private void registerTransformationService(String className) throws IncompatibleEnvironmentException {
-        try {
-            Class<? extends ITransformationService> transformationServiceClass = (Class<? extends ITransformationService>) Class.forName(className, true, Thread.currentThread().getContextClassLoader());
-            if (isTransformationServicePresent(transformationServiceClass)) {
-                LOGGER.warn("{} is already registered", transformationServiceClass.getSimpleName());
-                return;
-            }
-            
-            ITransformationService transformationService = transformationServiceClass.newInstance();
-            String name = transformationService.name();
-            this.transformationServices.add(transformationService);
-            LOGGER.debug("Registered {} ({})", transformationServiceClass.getSimpleName(), name);
-        } catch (Exception ex) {
-            LOGGER.error("Encountered an error while registering {}", className, ex);
-            throw new IncompatibleEnvironmentException(String.format("Failed to register %s", className));
-        }
-    }
-    
-    private boolean isLaunchPluginServicePresent(Class<? extends ILaunchPluginService> launchPluginServiceClass) {
-        for (ILaunchPluginService launchPluginService : this.launchPluginServices.values()) {
-            if (launchPluginServiceClass.isInstance(launchPluginService)) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    private boolean isTransformationServicePresent(Class<? extends ITransformationService> transformationServiceClass) {
-        for (ITransformationService transformationService : this.transformationServices) {
-            if (transformationServiceClass.isInstance(transformationService)) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    @SuppressWarnings("unchecked")
-    private Map<String, ILaunchPluginService> getLaunchPluginServices() {
-        try {
-            // cpw.mods.modlauncher.Launcher.launchPlugins
-            Field launchPluginsField = Launcher.class.getDeclaredField("launchPlugins");
-            launchPluginsField.setAccessible(true);
-            LaunchPluginHandler launchPluginHandler = (LaunchPluginHandler) launchPluginsField.get(Launcher.INSTANCE);
-            
-            // cpw.mods.modlauncher.LaunchPluginHandler.plugins
-            Field pluginsField = LaunchPluginHandler.class.getDeclaredField("plugins");
-            pluginsField.setAccessible(true);
-            return (Map<String, ILaunchPluginService>) pluginsField.get(launchPluginHandler);
-        } catch (Exception ex) {
-            LOGGER.error("Encountered an error while getting LaunchPluginServices", ex);
-            return null;
-        }
-    }
-    
+
+
     @SuppressWarnings("unchecked")
     private List<Path> getTransformers() {
         try {
             Class<?> modDirTransformerDiscovererClass = Class.forName("net.minecraftforge.fml.loading.ModDirTransformerDiscoverer", true, Launcher.class.getClassLoader());
-            
+
             // net.minecraftforge.fml.loading.ModDirTransformerDiscoverer.transformers
             Field transformersField = modDirTransformerDiscovererClass.getDeclaredField("transformers");
             transformersField.setAccessible(true);
